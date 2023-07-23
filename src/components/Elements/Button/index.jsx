b187abb580ce766856994d8e0961846febd28b04
Fazrilutama <utamafazril8@gmail.com>
2023-07-23 18:25:38 +0700

    menambahkan clas componen

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Button = (props) => {
-    const {children, classname = "bg-black"} = props
+  const { children, classname = "bg-black", onClick = () => {}, type = "button" } = props;
   return (
-    <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type='submit'>{children} </button>
-  )
-}
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`}
+      type={type}
+      onClick={onClick}
+    >
+      {children}{" "}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import Button from '../Elements/Button'
-import InputForm from '../Elements/Input/Index'
+import React from "react";
+import Button from "../Elements/Button";
+import InputForm from "../Elements/Input/Index";
 
 const FormLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    localStorage.setItem('email', event.target.email.value );
+    localStorage.setItem('password', event.target.password.value );
+    console.log("login");
+    window.location.href = "/products";
+  };
   return (
-    <form action="">
-          <InputForm
-            label="email"
-            type="email"
-            placeholder="example@gmail.com"
-            name="email"
-          />
-          <InputForm
-            label="password"
-            type="password"
-            placeholder="******"
-            name="password"
-          />
-          <Button classname="bg-blue-600 w-full">Login</Button>
-        </form>
+    <form onSubmit={handleLogin}>
+      <InputForm
+        label="email"
+        type="email"
+        placeholder="example@gmail.com"
+        name="email"
+      />
+      <InputForm
+        label="password"
+        type="password"
+        placeholder="******"
+        name="password"
+      />
+      <Button classname="bg-blue-600 w-full" type="submit ">
+        Login
+      </Button>
+    </form>
+  );
+};
 
-  )
-}
-
-export default FormLogin
+export default FormLogin;
